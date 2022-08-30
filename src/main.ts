f428e24643b74eb548f7d40de54c1a686841ee9b
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-08-30 20:09:29 +0900

    add: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 import { PrismaService } from './services/prisma.service';
 
@@ -6,6 +8,17 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
+  // swagger
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  // prisma
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app)
 
