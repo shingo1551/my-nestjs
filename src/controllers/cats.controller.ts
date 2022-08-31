36fdecc7c904ee72010d9420b114470f6e9b9c91
ShingoTanikawa <shingo.tanikawa@o-n.co.jp>
2022-08-31 10:12:10 +0900

    FastifyRequest

--- src/controllers/cats.controller.ts
+++ src/controllers/cats.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Req } from '@nestjs/common';
+import { FastifyRequest } from 'fastify';
 
 @Controller('cats')
 export class CatsController {
   @Get()
-  findAll(@Req() request): string {
+  findAll(@Req() request: FastifyRequest): string {
     return 'This action returns all cats';
   }
 }
